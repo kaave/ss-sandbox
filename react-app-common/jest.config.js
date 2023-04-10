99d37c00de5e87241ea3f7c359fed9e44b15ccea
kaave <junkjunctions@gmail.com>
2023-04-10 21:49:15 +0900

    Change from babel to swc

--- react-app-common/jest.config.js
+++ react-app-common/jest.config.js
@@ -2,7 +2,22 @@ module.exports = {
   rootDir: "src",
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.(j|t)sx?$": "babel-jest",
+    "^.+\\.(j|t)sx?$": ["@swc/jest", {
+        sourceMaps: true,
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          transform: {
+            react: {
+              // 必須。省略すると "ReferenceError: React is not defined" が発生
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
   },
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
--- react-app-common/webpack.config.js
+++ react-app-common/webpack.config.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
+const isDevelopmentMode = process.env.NODE_ENV !== 'production';
+
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "kaave",
@@ -9,8 +11,37 @@ module.exports = (webpackConfigEnv, argv) => {
     argv,
   });
 
+  // Note: TS 関連が混じっていると面倒を生むので排除する
+  defaultConfig.module.rules = defaultConfig.module.rules.filter(({ test }) => !test.test('.ts'))
+
   return merge(defaultConfig, {
     // this project self deps packages.
     externals: {},
+    module: {
+      rules: [
+        {
+          test: /\.(j|t)sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "swc-loader",
+            options: {
+              sourceMaps: isDevelopmentMode,
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    // for "ReferenceError: React is not defined" error
+                    runtime: "automatic",
+                  },
+                },
+              },
+            },
+          },
+        },
+      ],
+    }
   });
 };
