48effd99a67a0885f4a3e99185620cc629d388ac
kaave <junkjunctions@gmail.com>
2023-04-11 22:44:12 +0900

    change to swc

--- application-shell/webpack.config.js
+++ application-shell/webpack.config.js
@@ -4,6 +4,8 @@ const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+const isDevelopmentMode = process.env.NODE_ENV !== 'production';
+
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "kaave";
   const defaultConfig = singleSpaDefaults({
@@ -14,11 +16,40 @@ module.exports = (webpackConfigEnv, argv) => {
     disableHtmlGeneration: true,
   });
 
+  // Note: TS 関連が混じっていると面倒を生むので排除する
+  defaultConfig.module.rules = defaultConfig.module.rules.filter(({ test }) => !test.test('.ts'))
+
   // Note: React は独自に読みこんでほしいので排除する
   defaultConfig.externals = defaultConfig.externals.filter(external => !/^react(-dom)?$/.test(external));
 
   return merge(defaultConfig, {
     externalsType: 'system',
+    module: {
+      rules: [
+        {
+          test: /\.(j|t)sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "swc-loader",
+            options: {
+              sourceMaps: isDevelopmentMode,
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    // for "ReferenceError: React is not defined" error
+                    runtime: "automatic",
+                  },
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
