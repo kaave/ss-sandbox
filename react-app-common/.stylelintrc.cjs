51595c878635b79c66c5c2f22b7c3518f79df746
kaave <junkjunctions@gmail.com>
2023-04-13 23:00:27 +0900

    add stylelint plugin

--- react-app-common/.stylelintrc.cjs
+++ react-app-common/.stylelintrc.cjs
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['stylelint-config-recess-order', 'stylelint-config-recommended', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended',
+    'stylelint-config-css-modules',
+    'stylelint-a11y',
+    'stylelint-config-prettier',
+  ],
   plugins: ['stylelint-declaration-block-no-ignored-properties'],
   ignoreFiles: ['node_modules/**/*', 'dist/**/*'],
   rules: {
@@ -20,12 +26,18 @@ module.exports = {
         ignore: ['after-comment', 'stylelint-commands'],
       },
     ],
-    // CSS Modules用の記法を許可
-    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'local', 'export'] }],
     // 不正なCSSはNGだが、exportの中はなんでもあり
-    'property-no-unknown': [true, { ignoreSelectors: [':export'] }],
+    'property-no-unknown': true,
 
     // `display: inline; width: 100px;` などの無効な組み合わせを注意する。
     'plugin/declaration-block-no-ignored-properties': true,
+
+    /*
+     * a11y
+     */
+    // `outline` の単純な非表示化を禁止する。 border などの代替手段を用意すればOK。
+    'a11y/no-outline-none': true,
+    // :hover のみなにかを処理することを禁止する。 :focus をセットにすること。
+    'a11y/selector-pseudo-class-focus': true,
   },
 };
