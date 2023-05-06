0720dc4c51e4567c49b5a863761ed577c0f26236
kaave <junkjunctions@gmail.com>
2023-05-06 23:38:02 +0900

    Improve scripts

--- react-app-common/src/bootstrap.spec.tsx
+++ react-app-common/src/bootstrap.spec.tsx
@@ -1,9 +1,9 @@
 import { screen, render } from '@testing-library/react';
 import { Root } from './bootstrap';
 
-// describe('Root component', () => {
-//   it('should be in the document', () => {
-//     render(<Root />);
-//     expect(screen.getByText(/root\./i)).toBeInTheDocument();
-//   });
-// });
+describe('Root component', () => {
+  it('should be in the document', () => {
+    render(<Root />);
+    expect(screen.getByText(/root\./i)).toBeInTheDocument();
+  });
+});
