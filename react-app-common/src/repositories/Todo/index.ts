9ccbdb153ac3a915702a3f12ac2c636f2f555833
kaave <junkjunctions@gmail.com>
2023-05-09 00:46:37 +0900

    Improve example domain

--- react-app-common/src/repositories/Todo/index.ts
+++ react-app-common/src/repositories/Todo/index.ts
@@ -1,21 +1,64 @@
-import { createPromiseClient, type Transport } from '@bufbuild/connect';
+import { createPromiseClient } from '@bufbuild/connect';
+import type { PromiseClient, Transport } from '@bufbuild/connect';
 import { createConnectTransport } from '@bufbuild/connect-web';
 import { TodoService } from '../../../gen/schemas/v1/todo_connect';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { todo, type Todo, type TodoBody, type TodoId } from '../../domains/models/Todo';
 
+type TodoRepositories = {
+  /**
+   * Todo の一覧を取得する非同期関数。
+   *
+   * @returns Todo の一覧または Error。
+   */
+  indexTodo: () => Promise<Error | Todo[]>;
+  /**
+   * Todo を新規追加する非同期関数。
+   *
+   * @param body Todo の本文。
+   *
+   * @returns 追加された Todo または Error。
+   */
+  createTodo: (body: TodoBody) => Promise<Todo | Error>;
+  /**
+   * 対象の Todo を更新する。
+   *
+   * @param updateTodo 更新対象の Todo。
+   *
+   * @returns 更新後の Todo または Error。
+   */
+  updateTodo: (updateTodo: Readonly<Todo>) => Promise<Todo | Error>;
+  /**
+   * 対象の Todo を削除する。
+   *
+   * @param id 削除対象の Todo の ID。
+   */
+  destroyTodo: (id: TodoId) => Promise<void>;
+};
+
 /**
- * Todo の一覧を取得する関数を作成する。
+ * Connect が生成した関数を純粋な関数として変換したものを利用するための Hooks。
  *
  * @param transport {@link createConnectTransport} で生成した {@link Transport}。
  *
- * @returns Todo の一覧を取得する非同期関数。
+ * @returns `TodoService.methods` を純粋な関数として利用できるようにしたもの。
  */
-export async function useIndexTodo(transport: Transport): Promise<{ indexTodo: () => Promise<Todo[] | Error> }> {
+export function useTodoRepositories(transport: Transport): TodoRepositories {
   const todoClient = useMemo(() => createPromiseClient(TodoService, transport), [transport]);
 
+  return useMemo(() => createTodoRepositories(todoClient), [todoClient]);
+}
+
+/**
+ * Client を元に TodoRepositories を生成する。
+ *
+ * @param todoClient クライアント
+ *
+ * @returns リポジトリ
+ */
+function createTodoRepositories(todoClient: PromiseClient<typeof TodoService>): TodoRepositories {
   return {
-    indexTodo: useCallback(async () => {
+    indexTodo: async () => {
       const responseTodos = await todoClient.indexTodo({});
       const todos: Todo[] = [];
 
@@ -31,86 +74,35 @@ export async function useIndexTodo(transport: Transport): Promise<{ indexTodo: (
       }
 
       return todos;
-    }, [todoClient]),
-  };
+    },
+    createTodo: async (body: TodoBody) => {
+      const responseTodo = await todoClient.createTodo({ body });
+
+      return responseTodo.todo === undefined
+        ? new Error('Failed to create todo')
+        : todo({
+            id: responseTodo.todo.id,
+            body: responseTodo.todo.body,
+            archived: responseTodo.todo.archived,
+          });
+    },
+    updateTodo: async (updateTodo: Readonly<Todo>) => {
+      const responseTodo = await todoClient.updateTodo({ todo: updateTodo });
+
+      return responseTodo.todo === undefined
+        ? new Error('Failed to update todo')
+        : todo({
+            id: responseTodo.todo.id,
+            body: responseTodo.todo.body,
+            archived: responseTodo.todo.archived,
+          });
+    },
+    destroyTodo: async (id: TodoId) => {
+      await todoClient.destroyTodo({ id });
+    },
+    // すべての Service を網羅していることを明示的にするために、 satisfies を活用してチェックしている。
+  } satisfies Record<keyof typeof TodoService.methods, (...args: unknown[]) => unknown>;
 }
 
-/**
- * Todo を生成する関数を作成する。
- *
- * @param transport {@link createConnectTransport} で生成した {@link Transport}。
- *
- * @returns Todo の一覧を取得する非同期関数。
- */
-export async function useCreateTodo(
-  transport: Transport,
-): Promise<{ createTodo: (body: TodoBody) => Promise<Todo | Error> }> {
-  const todoClient = useMemo(() => createPromiseClient(TodoService, transport), [transport]);
-
-  return {
-    createTodo: useCallback(
-      async (body: TodoBody) => {
-        const responseTodo = await todoClient.createTodo({ body });
-
-        return responseTodo.todo === undefined
-          ? new Error('Failed to create todo')
-          : todo({
-              id: responseTodo.todo.id,
-              body: responseTodo.todo.body,
-              archived: responseTodo.todo.archived,
-            });
-      },
-      [todoClient],
-    ),
-  };
-}
-
-/**
- * Todo を更新する関数を作成する。
- *
- * @param transport {@link createConnectTransport} で生成した {@link Transport}。
- *
- * @returns Todo を更新する非同期関数。
- */
-export async function useUpdateTodo(
-  transport: Transport,
-): Promise<{ updateTodo: (updateTodo: Readonly<Todo>) => Promise<Todo | Error> }> {
-  const todoClient = useMemo(() => createPromiseClient(TodoService, transport), [transport]);
-
-  return {
-    updateTodo: useCallback(
-      async (updateTodo: Readonly<Todo>) => {
-        const responseTodo = await todoClient.updateTodo({ todo: updateTodo });
-
-        return responseTodo.todo === undefined
-          ? new Error('Failed to update todo')
-          : todo({
-              id: responseTodo.todo.id,
-              body: responseTodo.todo.body,
-              archived: responseTodo.todo.archived,
-            });
-      },
-      [todoClient],
-    ),
-  };
-}
-
-/**
- * Todo を削除する関数を作成する。
- *
- * @param transport {@link createConnectTransport} で生成した {@link Transport}。
- *
- * @returns Todo を削除する非同期関数。
- */
-export async function useDestroyTodo(transport: Transport): Promise<{ destroyTodo: (id: TodoId) => Promise<void> }> {
-  const todoClient = useMemo(() => createPromiseClient(TodoService, transport), [transport]);
-
-  return {
-    destroyTodo: useCallback(
-      async (id: TodoId) => {
-        await todoClient.destroyTodo({ id });
-      },
-      [todoClient],
-    ),
-  };
-}
+/** @deprecated {@link createTodoRepositories} をテストするためのもの。通常利用しないこと。 */
+export const createTodoRepositoriesForTest = createTodoRepositories;
